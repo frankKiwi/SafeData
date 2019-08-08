3f9d2a4b7ba69b23f0aeaf3343688cfa4267d082
fanrenFRank <1778907544@qq.com>
2019-08-08 14:00:51 +0800

    first commit for version 1.0.0

---
+++ SafeContainer/NSArray+Safe.h
@@ -0,0 +1,13 @@
+//
+//  NSArray+Safe.h
+//  NSArrayTest
+//
+//  Created by FNK on 2017/11/28.
+//  Copyright © 2017年 FNK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (Safe)
+
+@end
