93b2f79eaa5700bb60e9968538e737f23f5017d4
fanrenFRank <1778907544@qq.com>
2019-08-12 10:48:38 +0800

    ss

---
+++ SafeContainer/NSNull+NullSafe.h
@@ -0,0 +1,13 @@
+//
+//  NSNull+NullSafe.h
+//  FNK
+//
+//  Created by FNK on 2017/5/15.
+//  Copyright © 2017年 LWW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNull (NullSafe)
+
+@end
